b8e62f4b7b6f76af6c4942358b78e11cbdbacd96
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-24 17:25:54 -0300

    feat: Implementing alert component.

--- src/app/components/alert.tsx
+++ src/app/components/alert.tsx
@@ -1,9 +1,22 @@
 import React from "react"
+import styles from './styles/Alert.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 
-export default function Alert() {
+export default function Alert({ message }: { message: string }) {
+  if (!message) return null;
+
+  const isError = message.toLowerCase().includes("erro");
+  const alertStyle = isError ? styles.error : styles.success;
+  const icon = isError ? faXmark : faCheck;
+
   return (
-    <div>
-      Alert
+    <div className={`${styles.alert} ${alertStyle}`}>
+      <div className={`${styles.marker} ${isError ? styles.red : styles.green}`}></div>
+      <div className={`flex ${styles.message}`}>
+        <FontAwesomeIcon icon={icon} className={styles.icon} />
+        {message}
+      </div>
     </div>
-  )
+  );
 }
