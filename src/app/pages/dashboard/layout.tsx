7bf36fbec7edd8e6cb44bfa8639bd7b6ff1dfaea
Maisa Preis <maisapierinipreis@gmail.com>
2024-08-29 21:21:58 -0300

    fix: Import not being used.

--- src/app/pages/dashboard/layout.tsx
+++ src/app/pages/dashboard/layout.tsx
@@ -1,9 +1,7 @@
-import styles from './styles.module.css'
- 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <section className={styles.dashboard}>{children}</section>
+  return <section>{children}</section>
 }
