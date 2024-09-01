3966b720f00250ebbeffe4e3f2a13bc013b2f175
Maisa Preis <maisapierinipreis@gmail.com>
2024-09-01 19:59:02 -0300

    feat: Structuring the agenda to enable connection to the backend soon.

--- src/app/pages/calendar/appointments.tsx
+++ src/app/pages/calendar/appointments.tsx
@@ -1,27 +1,84 @@
+"use client";
+import { useState } from "react";
 import styles from "./Calendar.module.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import Modal from "@/app/components/modal";
+import { formatDate } from "@/utils/date";
+
+interface PatientProps {
+  date: string;
+  time: string;
+  name: string;
+  notes: string;
+}
 
 interface AppointmentsProps {
   time: string;
-  patients: string[];
+  patients: PatientProps[];
 }
 
 export default function Appointments({ time, patients }: AppointmentsProps) {
+  const [showModal, setShowModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [selectedPatient, setSelectedPatient] = useState({
+    date: "",
+    time: "",
+    name: "",
+    notes: ""
+  });
+
+  const openModal = (patient: PatientProps): void => {
+    if (patient.name !== "") {
+      setSelectedPatient(patient);
+      console.log('patient', patient)
+    }
+    setShowModal(true);
+    setModalTitle("Agendamento");
+
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedPatient({
+      date: "",
+      time: "",
+      name: "",
+      notes: ""
+    });
+  };
+
+  const saveAppointment = () => {
+    console.log('Salvar')
+  }
+
   return (
     <>
       <div className={`${styles.schedule} ${styles.blue} ${styles.text}`}>{time}</div>
       {patients.map((patient, index) => (
-        <button key={index} className={`${styles.schedule} ${styles.graylight}`}>
+        <button key={index} className={`${styles.schedule} ${styles.graylight}`} onClick={() => openModal(patient)}>
           <div className="flex justify-center">
-            <p className={`${styles.text}`}>{patient}</p>
-            {/* {patient ?
-              <FontAwesomeIcon icon={faCircleInfo} className={styles.info} />
-              : <span></span>
-            } */}
+            <p className={`${styles.text}`}>{patient.name}</p>
           </div>
         </button>
       ))}
+
+      {showModal &&
+        <Modal title={modalTitle}>
+          <div className="my-5 text-left">
+            <h3 className="mb-3">Dia: <strong>{formatDate(selectedPatient.date)}</strong></h3>
+            <h3 className="mb-3">Horário: <strong>{selectedPatient.time}</strong></h3>
+            <h3>Paciente: <strong>{selectedPatient.name}</strong></h3>
+            <p className="my-4">{selectedPatient.notes}</p>
+          </div>
+          <div className="flex justify-around">
+            <button onClick={saveAppointment} className="btn orange size">
+              Editar
+            </button>
+            <button onClick={closeModal} className="btn red size blue">
+              Fechar
+            </button>
+          </div>
+        </Modal>
+      }
     </>
   );
 }
--- src/app/pages/calendar/page.tsx
+++ src/app/pages/calendar/page.tsx
@@ -1,30 +1,75 @@
+"use client";
+import { useMemo } from "react";
 import styles from "./Calendar.module.css";
 import Appointments from "./appointments";
 
-const daysOfWeek = [
-  { dayWeek: "Seg", day: "18/03" },
-  { dayWeek: "Ter", day: "19/03" },
-  { dayWeek: "Qua", day: "20/03" },
-  { dayWeek: "Qui", day: "21/03" },
-  { dayWeek: "Sex", day: "22/03" },
-  { dayWeek: "Sáb", day: "23/03" },
-];
-
-// TODO: Receive the full name from the backend, and create a function to add a short name.
-
-const appointments = [
-  { time: "09:00", patients: ["", "", "Luis Silva", "", "Amanda Lop", ""] },
-  { time: "10:00", patients: ["", "Renan B.", "", "Beatriz Silva", "", ""] },
-  { time: "11:00", patients: ["", "", "Luis Silva", "", "Amanda Lopes", ""] },
-  { time: "13:00", patients: ["Bianca", "", "Paulo", "", "", ""] },
-  { time: "14:00", patients: ["", "", "", "", "", ""] },
-  { time: "15:00", patients: ["", "", "Luis Silva", "", "Amanda L.", ""] },
-  { time: "16:00", patients: ["", "José", "", "", "Lopes", ""] },
-  { time: "17:00", patients: ["", "", "Maisa Preis", "", "André Lopes", ""] },
-  { time: "18:00", patients: ["", "", "", "", "Bia", ""] },
+const dataComingFromBack = [
+  { date: "2024-09-02", time: "09:00", patient: "Luis Silva", notes: "" },
+  { date: "2024-09-02", time: "10:00", patient: "Renan Bern", notes: "" },
+  { date: "2024-09-03", time: "11:00", patient: "Amanda Lopes", notes: "" },
+  { date: "2024-09-03", time: "13:00", patient: "Bianca", notes: "" },
+  { date: "2024-09-03", time: "14:00", patient: "Duff Preis", notes: "Muito lindo" },
+  { date: "2024-09-04", time: "15:00", patient: "Luis Silva", notes: "" },
+  { date: "2024-09-04", time: "16:00", patient: "José", notes: "" },
+  { date: "2024-09-04", time: "17:00", patient: "Maisa Preis", notes: "" },
+  { date: "2024-09-05", time: "18:00", patient: "Bia", notes: "" },
 ]
 
+const times = ["09:00", "10:00", "11:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
+
+const initialFormat = [
+  { date: "", time: "", name: "", notes: "" },
+  { date: "", time: "", name: "", notes: "" },
+  { date: "", time: "", name: "", notes: "" },
+  { date: "", time: "", name: "", notes: "" },
+  { date: "", time: "", name: "", notes: "" },
+  { date: "", time: "", name: "", notes: "" },
+];
+
 export default function Calendar() {
+
+  const daysOfWeek = useMemo(() => {
+    const today = new Date();
+    const startOfWeek = today.getDate() - today.getDay() + 1;
+    const days = ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+
+    return days.map((day, index) => {
+      const currentDay = new Date(today.setDate(startOfWeek + index));
+      const dayFormatted = currentDay.toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+      });
+
+      return { dayWeek: day, day: dayFormatted, date: currentDay.toISOString().split('T')[0] };
+    });
+  }, []);
+
+  const initialAppointments = times.map(time => ({
+    time,
+    patients: initialFormat.map(patient => ({ ...patient, time }))
+  }));
+
+  const appointments = useMemo(() => {
+    const updatedAppointments = [...initialAppointments];
+
+    dataComingFromBack.forEach(({ date, time, patient, notes }) => {
+      const dayIndex = daysOfWeek.findIndex(day => day.date === date);
+      if (dayIndex !== -1) {
+        const appointment = updatedAppointments.find(a => a.time === time);
+        if (appointment) {
+          appointment.patients[dayIndex] = {
+            date,
+            time,
+            name: patient,
+            notes: notes,
+          };
+        }
+      }
+    });
+
+    return updatedAppointments;
+  }, [daysOfWeek, initialAppointments]);
+
   return (
     <div className="content">
       <div className={styles.grid}>
