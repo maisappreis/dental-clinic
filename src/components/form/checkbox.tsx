f4179fa07f2f393e7226766d615392d8f3d9202d
Maisa Preis <maisapierinipreis@gmail.com>
2026-02-25 12:28:01 -0300

    feat: Creating Form components and applying it to the components.

---
+++ src/components/form/checkbox.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { forwardRef, InputHTMLAttributes, useId } from "react";
+import styles from "./css/Checkbox.module.css";
+
+interface CheckboxProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange" | "checked"> {
+  label?: string;
+  checked?: boolean;
+  error?: string;
+  onChange?: (checked: boolean) => void;
+}
+
+export const Checkbox = forwardRef<
+  HTMLInputElement,
+  CheckboxProps
+>(function Checkbox(
+  { label, checked = false, error, id, onChange, ...rest },
+  ref
+) {
+  const generatedId = useId();
+  const checkboxId = id ?? generatedId;
+  const errorId = error ? `${checkboxId}-error` : undefined;
+
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={checkboxId} className={styles.labelWrapper}>
+        <input
+          ref={ref}
+          id={checkboxId}
+          type="checkbox"
+          checked={checked}
+          aria-invalid={!!error}
+          aria-describedby={errorId}
+          className={styles.input}
+          onChange={(e) => onChange?.(e.target.checked)}
+          {...rest}
+        />
+
+        {label && <span className={styles.label}>{label}</span>}
+      </label>
+
+      {error && (
+        <span id={errorId} className={styles.errorMessage}>
+          {error}
+        </span>
+      )}
+    </div>
+  );
+});
