a6f52aba52ad23bb15ccfb389d05b15db66179c5
Maisa Preis <maisapierinipreis@gmail.com>
2026-03-09 16:16:28 -0300

    fix: Prodution base api.

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import { API_URL } from "@/config/env";
 
-export const apiBase = "http://localhost:8000/api";
-// export const apiBase: string = "https://django-apis-two.vercel.app/api";
+// export const apiBase = "http://localhost:8000/api";
+export const apiBase: string = "https://django-apis-two.vercel.app/api";
 
 export const isAuthenticated = (): boolean => {
   return Boolean(localStorage.getItem("accessToken"));
