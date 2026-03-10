598e8335ea31a0beed07d6f5c2abe06d899ec273
Maisa Preis <maisapierinipreis@gmail.com>
2026-03-10 20:17:31 -0300

    fix: Vercel api in production.

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,5 +1,5 @@
-export const apiBase = "http://localhost:8000/api";
-// export const apiBase: string = "https://django-apis-two.vercel.app/api";
+// export const apiBase = "http://localhost:8000/api";
+export const apiBase: string = "https://django-apis-two.vercel.app/api";
 
 export const apiURL = (): string => {
   return `${apiBase}/dental`;
